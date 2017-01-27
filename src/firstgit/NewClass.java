192589a88864dbcb979b3ceccf2725a35a40ee9e
jordanrehbein <jordanrehbein@10.30.22.253>
2017-01-26 21:04:12 -0600

    Added Var and Message Change

--- src/firstgit/NewClass.java
+++ src/firstgit/NewClass.java
@@ -9,15 +9,6 @@ package firstgit;
  *
  * @author jordanrehbein
  */
-public class FirstGit {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello world");
-        int x =5;
-    }
+public class NewClass {
     
 }
